f51fcae84333089961042e0d82895bc8b1e19358
shubham <shubhamsagar628@gmail.com>
2024-04-18 21:28:29 +0530

    Done

---
+++ 05.js
@@ -0,0 +1,11 @@
+// Write a function to sort an array of numbers in an ascending order.
+
+
+
+ 
+const sortingFun = (arr) => {
+    return arr.sort((a,b)=> b-a)
+
+}
+
+console.log(sortingFun([8,1,2,3,6]))
